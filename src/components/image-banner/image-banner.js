2f6b8434e7ab20a6c6b78ea81362424b4b63ac8a
Robin <ameego@gmail.com>
2020-08-07 14:08:02 +0800

    Remove eager loading for banner

--- src/components/image-banner/image-banner.js
+++ src/components/image-banner/image-banner.js
@@ -5,7 +5,7 @@ import style from "./image-banner.module.scss"
 const ImageBanner = ({ data }) => (
   <div className={style.banner}>
     <div className={style.banner__img}>
-      <FluidImg durationFadeIn={5000} loading="eager" data={data} />
+      <FluidImg durationFadeIn={5000} data={data} />
     </div>
   </div>
 )
