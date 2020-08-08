5915d5962e0fec758caf98650288f3b538066d9e
Robin <ameego@gmail.com>
2020-08-08 14:37:02 +0800

    Leverage page-intro in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery } from "gatsby"
 import Layout from "../components/layout/layout"
+import PageIntro from "../components/page-intro/page-intro"
 import ArtistList from "../components/artist-list/artist-list"
 import ImageBanner from "../components/image-banner/image-banner"
 
@@ -29,6 +30,10 @@ export const Home = () => {
     <>
       <ImageBanner data={data.allHomeJson.edges[0].node.banner} />
       <Layout>
+        <PageIntro
+          title={data.allHomeJson.edges[0].node.title}
+          subtitle={data.allHomeJson.edges[0].node.subtitle}
+        />
         <ArtistList data={data} />
       </Layout>
     </>
