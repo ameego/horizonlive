f8cf3f42ea99045158b4f45e975a02637fff11c9
Robin <ameego@gmail.com>
2020-08-08 14:45:48 +0800

    Styles as module in page-intro component

--- src/components/page-intro/page-intro.js
+++ src/components/page-intro/page-intro.js
@@ -1,12 +1,11 @@
 import React from "react"
+import style from "./page-intro.module.scss"
 
 const PageIntro = ({ title, subtitle, children }) => (
-  <div className="formatted-content" style={{ width: "60%" }}>
-    <div className="formatted-content__introduction">
-      <h2 className="title-1">{title}</h2>
-      {children}
-      <h3>{subtitle}</h3>
-    </div>
+  <div className={style.container} style={{ width: "60%" }}>
+    <h2 className={style.title}>{title}</h2>
+    {children}
+    <h3 className={style.subtitle}>{subtitle}</h3>
   </div>
 )
 
