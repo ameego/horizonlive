9f0a57eec0cf5357a5223b44c2ced17d11a3b2d9
Robin <ameego@gmail.com>
2020-08-13 17:52:37 +0800

    Minor refactoring

--- src/components/fluid-img/fluid-img.js
+++ src/components/fluid-img/fluid-img.js
@@ -2,13 +2,11 @@ import React from "react"
 import Img from "gatsby-image"
 
 const FluidImg = ({ src, loading, durationFadeIn }) => (
-  <>
-    <Img
-      durationFadeIn={durationFadeIn ? durationFadeIn : 1500}
-      loading={loading ? loading : "lazy"}
-      fluid={src}
-    />
-  </>
+  <Img
+    durationFadeIn={durationFadeIn ? durationFadeIn : 1500}
+    loading={loading ? loading : "lazy"}
+    fluid={src}
+  />
 )
 
 export default FluidImg
