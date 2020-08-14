44a433c80e5c7b54d95e3fe44456ec2033ecdb1d
Robin <ameego@gmail.com>
2020-08-14 08:52:37 +0800

    Remove unused sticky header for now

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
@@ -6,26 +6,6 @@ import Navigation from "../navigation/navigation"
 import style from "./header.module.scss"
 
 const Header = ({ siteTitle }) => {
-  // useEffect(() => {
-  //   var observer = new IntersectionObserver(
-  //     function (entries) {
-  //       // no intersection with screen
-  //       if (entries[0].intersectionRatio === 0)
-  //         document
-  //           .querySelector("#nav-container")
-  //           .classList.add("nav-container-sticky")
-  //       // fully intersects with screen
-  //       else if (entries[0].intersectionRatio === 1)
-  //         document
-  //           .querySelector("#nav-container")
-  //           .classList.remove("nav-container-sticky")
-  //     },
-  //     { threshold: [0, 1] }
-  //   )
-
-  //   observer.observe(document.querySelector("#nav-container-top"))
-  // })
-
   return (
     <>
       <header id="nav-container" className={style.header}>
