0241e64622a7aa2d2a64355a3aacd147c99351b6
Robin <ameego@gmail.com>
2020-08-19 13:48:37 +0800

    Better integration of close button

--- src/components/audio-player/audio-player.js
+++ src/components/audio-player/audio-player.js
@@ -48,11 +48,6 @@ const Player = ({ data, canBeDismissed, isArtistPage }) => {
           : style.player
       }
     >
-      {!isArtistPage && canBeDismissed ? (
-        <div className={style.close}>
-          <button onClick={closePlayer}>Close</button>
-        </div>
-      ) : null}
       <AudioPlayer
         ref={player}
         layout="horizontal-reverse"
@@ -67,6 +62,22 @@ const Player = ({ data, canBeDismissed, isArtistPage }) => {
         onClickNext={handleClickNext}
         onPlay={onPlay}
       />
+
+      <div
+        className={
+          !isArtistPage && canBeDismissed
+            ? `${style.close} active`
+            : style.close
+        }
+      >
+        <button onClick={closePlayer}>
+          <svg width="32" height="32" viewBox="0 0 32 32">
+            <g transform="scale(0.03125 0.03125)">
+              <path d="M512 928c-229.76 0-416-186.24-416-416s186.24-416 416-416 416 186.24 416 416-186.24 416-416 416zM702.752 390.688c7.808-7.808 7.808-20.512 0-28.32l-42.496-42.464c-7.808-7.808-20.512-7.808-28.32 0l-120.352 120.352-120.352-120.352c-7.808-7.808-20.512-7.808-28.32 0l-42.496 42.464c-7.808 7.808-7.808 20.512 0 28.32l120.384 120.384-120.384 120.32c-7.808 7.808-7.808 20.512 0 28.32l42.496 42.496c7.808 7.808 20.512 7.808 28.32 0l120.352-120.384 120.352 120.384c7.808 7.808 20.512 7.808 28.32 0l42.496-42.496c7.808-7.808 7.808-20.512 0-28.32l-120.384-120.32 120.384-120.384z" />
+            </g>
+          </svg>
+        </button>
+      </div>
     </div>
   )
 }
