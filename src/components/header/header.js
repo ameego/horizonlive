310328bcd5541a7a1689c8838f3ceb9c83e08ac2
Robin <ameego@gmail.com>
2020-09-12 16:34:10 +0800

    Sticky header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,23 @@
 import React from "react"
-
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
+import Headroom from "react-headroom"
 import Navigation from "../navigation/navigation"
 import style from "./header.module.scss"
 
 const Header = ({ siteTitle }) => {
   return (
     <>
-      <header id="nav-container" className={style.header}>
-        <div>
-          <h1>
-            <Link to="/">{siteTitle}</Link>
-          </h1>
-          <Navigation />
-        </div>
-      </header>
+      <Headroom disableInlineStyles>
+        <header id="nav-container" className={style.header}>
+          <div>
+            <h1>
+              <Link to="/">{siteTitle}</Link>
+            </h1>
+            <Navigation />
+          </div>
+        </header>
+      </Headroom>
     </>
   )
 }
